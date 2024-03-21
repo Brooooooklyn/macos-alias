18e58edcac7c3ff23f36f4d9ae37c2af13dec4be
Peng Xiao <pengxiao@outlook.com>
2024-03-21 11:49:55 +0800

    fix: extra field encoding (#3)

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -1,17 +1,29 @@
-import { join, parse } from 'node:path'
+import { parse } from 'node:path';
 
-import test from 'ava'
-import alias from 'macos-alias'
+import test from 'ava';
+import alias from 'macos-alias';
 
-import { create } from '../index.js'
-import { fileURLToPath } from 'node:url'
+import { create } from '../index.js';
+import { fileURLToPath } from 'node:url';
 
-const selfpath = fileURLToPath(import.meta.url)
+const selfpath = fileURLToPath(import.meta.url);
 
 test('create should work', (t) => {
-  const buf = create(selfpath)
-  const info = alias.decode(buf)
+  const buf = create(selfpath);
+  const info = alias.decode(buf);
 
-  t.is('file', info.target.type)
-  t.is(parse(selfpath).base, info.target.filename)
-})
+  t.is('file', info.target.type);
+  t.is(parse(selfpath).base, info.target.filename);
+});
+
+if (process.arch === "arm64") {
+  // following test would fail on x64
+  test('create should work (check extra field)', (t) => {
+    const buf0 = alias.create(selfpath);
+    const buf1 = create(selfpath);
+    const info0 = alias.decode(buf0);
+    const info1 = alias.decode(buf1);
+  
+    t.deepEqual(info0.extra, info1.extra);
+  });
+}
